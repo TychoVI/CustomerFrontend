b92ab84311fe9628378a4b8d86456f1bb96e74d3
I461268 <d.picus@student.fontys.nl>
2021-03-22 14:07:02 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import WeatherForecast from './WeatherForecast';
+import {Landing} from "./Landing";
 
 function App() {
   return (
     <div className="App">
-      <WeatherForecast></WeatherForecast>
+      {/* <WeatherForecast></WeatherForecast> */}
+      <Router>
+        <Route path='/table/:id' component={Landing} />
+        <Route path='/test' component={WeatherForecast} />
+      </Router> 
     </div>
   );
 }
---
+++ src/Landing.js
@@ -0,0 +1,33 @@
+﻿import React, { Component } from 'react'
+import { 
+    BrowserRouter as Router, 
+    HashRouter, 
+    Route, 
+    Switch, useParams 
+} from 'react-router-dom';
+
+export class Landing extends Component {
+    static displayName = Landing.name;
+
+    render () {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <Router>
+                    <Switch>
+                        <Route path="/table/:id" children={<TableNr />}/>
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }  
+}
+
+function TableNr() {
+    let { id } = useParams(); 
+    return (
+      <div>
+        <h3>table {id}</h3>
+      </div>
+    );
+  }
