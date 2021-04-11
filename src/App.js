c879bd3ba3c3d0b477ab8c6e0a584a3e782f0277
i428866@fhict.nl <428866@student.fontys.nl>
2021-04-11 17:13:00 +0200

    increament/decreament counter added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import WeatherForecast from './WeatherForecast';
+import React, { Component } from 'react'
+import { Collapse } from 'react-collapse'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <WeatherForecast></WeatherForecast>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+	this.isOpen = false;
+    this.state = {
+      count: 0,
+    }
+  }
+  increament = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+  decreament = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 })
+    }
+  }
+
+  render() {
+    return (
+        <div className="app">
+          <label onClick="this.isOpened=true;">Sushi</label>
+          <button onClick={this.increament} className="counter">
+            +
+          </button>
+          <button onClick={this.decreament} className="counter">
+            -
+          </button>
+          <h2>{this.state.count}</h2>
+        </div>
+    )
+  }
 }
 
-export default App;
+export default App
