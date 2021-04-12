83c2af48eedbedc7589d83a819caef54eebb7bce
Jeroen Koeter <63229845+Jeroen-Koeter@users.noreply.github.com>
2021-04-12 11:47:34 +0200

    added npm library to generate components
    https://www.npmjs.com/package/reactjs-component-generator

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import Counter from './views/Counter'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
     <Counter />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 )
---
+++ src/placeOrder/placeOrder.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './placeOrder.scss'
+export default class placeOrder extends Component {
+  render() {    
+    return (
+        <div className="page-place-order">
+        Hello placeOrder 
+      </div>
+    )
+  }
+}
